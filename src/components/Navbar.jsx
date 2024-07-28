ba281a8535d4e12045acd5417ad084b3c3db6983
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-07-28 19:54:14 +0530

    simple nav complete

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { appleImg, bagImg, searchImg } from '../utils'
+import { navLists } from '../constants'
+const Navbar = () => {
+  return (
+    <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
+      <nav className='flex w-full screen-max-width'>
+        <img src={appleImg} alt='apple logo' width={14} height={18} />
+        <div className='flex flex-1 justify-center max-sm:hidden'>
+          {navLists.map((nav) => (
+            <div
+              key={nav}
+              className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'
+            >
+              {nav}
+            </div>
+          ))}
+        </div>
+        <div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
+          <img src={searchImg} alt='search' w={18} h={18} />
+          <img src={bagImg} alt='search' w={18} h={18} />
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
