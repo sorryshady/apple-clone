c41b1c71e2de6b6aa7eb9f1d2e6cb7cba94dd329
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-07-29 15:21:00 +0530

    trying to add sentry

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "akhil-nishad",
+    project: "iphone-clone"
+  }), sentryVitePlugin({
+    org: "akhil-nishad",
+    project: "iphone-clone"
+  }), sentryVitePlugin({
+    org: "akhil-nishad",
+    project: "iphone-clone"
+  }), sentryVitePlugin({
+    org: "akhil-nishad",
+    project: "iphone-clone"
+  }), sentryVitePlugin({
+    org: "akhil-nishad",
+    project: "iphone-clone"
+  }), sentryVitePlugin({
+    org: "akhil-nishad",
+    project: "javascript-react"
+  }), sentryVitePlugin({
+    org: "akhil-nishad",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
